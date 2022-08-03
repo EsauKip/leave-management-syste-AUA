7f66d89a74729bba95b3b034480b4f268522eca1
EsauKip <esau.kiprono@student.moringaschool.com>
2022-08-03 15:13:36 +0300

    code restructure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LeaveComponent } from './leave/leave.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {path:'dashboard',component:DashboardComponent},
+  {path:'leave',component:LeaveComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+interface sideNavToggle{
+  screenWidth: number;
+  collapsed: boolean;
+
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'aua-lms';
+  title = 'Leave Management System';
+  isSideNavCollapsed = false;
+  screenWidth =0;
+  onToggleSideNav(data:sideNavToggle): void {
+    this.screenWidth = data.screenWidth;
+    this.isSideNavCollapsed = data.collapsed;
+
+  }
 }
