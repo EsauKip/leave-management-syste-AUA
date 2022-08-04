ce0eca8aa23893cea06b312ff2c8b12cb538b4b5
EsauKip <esau.kiprono@student.moringaschool.com>
2022-08-04 10:16:13 +0300

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HolidaysComponent } from './holidays/holidays.component';
 import { LeaveComponent } from './leave/leave.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   {path:'dashboard',component:DashboardComponent},
   {path:'leave',component:LeaveComponent},
+  {path:'profile',component:ProfileComponent},
+  {path:'holidays',component:HolidaysComponent}
 ];
 
 @NgModule({
