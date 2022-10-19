74f5484d36dcfd58a9788bac9d1b3c266f45997d
EsauKip <esau.kiprono@student.moringaschool.com>
2022-10-19 12:00:01 +0300

    create sign up page

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
+signup:FormGroup|any
   constructor() { }
 
   ngOnInit(): void {
-  }
+this.signup = new FormGroup({
+  'fname':new FormControl(),
+  'email':new FormControl(),
+  'lname':new FormControl(),
+  'password':new FormControl(),
+  'phone':new FormControl(),
+   
+});
 
+  }
+signupdata(signup:FormGroup){
+console.log(this.signup.value)
+}
 }
