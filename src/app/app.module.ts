87c0c0e6c8386588d5ab2574fb72e840ce994e20
EsauKip <esau.kiprono@student.moringaschool.com>
2022-10-21 11:47:18 +0300

    store log in data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -41,7 +42,7 @@ import { SignupComponent } from './signup/signup.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
-   
+    HttpClientModule,
     MatCardModule,
     MatProgressBarModule,
     AddToCalendarModule
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+declare var $:any;
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,7 +10,7 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 login:FormGroup|any;
-  constructor() { }
+constructor(private _http:HttpClient, private _route:Router) { }
 
   ngOnInit(): void {
     this.login=new FormGroup({
@@ -17,6 +19,29 @@ login:FormGroup|any;
     });
   }
 logindata(login:FormGroup){
-console.log(this.login.value);
+// console.log(this.login.value);
+this._http.get<any>("http://localhost:3000/signup")
+    .subscribe(res=>{
+      const user = res.find((a:any)=>{
+        return a.fname === this.login.value.fname && a.password === this.login.value.password
+      });
+
+      if(user){
+        alert('you are successfully login');
+        this.login.reset();
+        $('.form-box').css('display','none');
+        this._route.navigate(['dashboard']);
+      }else{
+        alert('User Not Found');
+        this._route.navigate(['login']);
+      }
+
+    }, err=>{
+      alert('Something was wrong');
+    })
+}
+sbtn1(){
+  $('.form-box').css('display','none');
+  $('.form-box1').css('display','block');
 }
 }
